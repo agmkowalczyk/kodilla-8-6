1abc7bfb47d48170b596218ecb074b183936e3b0
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-09-20 19:48:54 +0200

    triangleArea added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // scripts.js
 
-var name = prompt('Enter your name');
+//var name = prompt('Enter your name');
 //alert('Hello, ' + name);
-console.log('Hello, ' + name);
+//console.log('Hello, ' + name);
 
 // komentarz 1
 
@@ -10,3 +10,10 @@ console.log('Hello, ' + name);
   komentarz 2
 
 */
+var a = prompt('Set \'a\' value'),
+  h = prompt('Set \'h\' value'),
+  //triangleArea;        //czy trzeba dac najpierw pustą zmienna?
+  triangleArea = a*h/2;  //czy też  mozna w deklaracji od razu dac takie rownanie?
+ 
+  console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+  alert('triangleArea is ' + triangleArea + '. Good job!');
