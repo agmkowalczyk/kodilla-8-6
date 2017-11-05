b90f5e170f197d36ec3ae1f84e6cf946a6e41a0f
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-05 04:02:13 +0100

    dodane uwagi mentora

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,8 @@
-// scripts.js
-
-//var name = prompt('Enter your name');
-//alert('Hello, ' + name);
-//console.log('Hello, ' + name);
-
-// komentarz 1
-
-/*
-  komentarz 2
-
-*/
-
-/*  ----- ZADANIE 8.5 -----
-var a = prompt('Set \'a\' value'),
-  h = prompt('Set \'h\' value'),
-  //triangleArea;        //czy trzeba dac najpierw pustą zmienna?
-  triangleArea = a*h/2;  //czy też  mozna w deklaracji od razu dac takie rownanie?
- 
-  console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-  alert('triangleArea is ' + triangleArea + '. Good job!');
-  */
 
 var a = prompt('Set "a" value'),
-  b = prompt('Set "b" value'),
-  value = (a*a) + (2*a*b) - (b*b);
-  //value = 0;
+    b = prompt('Set "b" value'),
+    value = (a*a) + (2*a*b) - (b*b);
+
 console.log('Value of "value" is: ' + value);
   
 if (value > 0) {
